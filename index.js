3597071506b394b122e3e70d5ab6217fc0fb87f4
Van Pho Nguyen <vanpho02@gmail.com>
2017-05-03 21:23:54 +0700

    upload

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const multer = require('multer');
 
-const uploadMiddle = multer({ dest: './public' }).single('avatar');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, './public'),
+    filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`)
+});
+
+const limits = { fileSize: 100 * 1024 };
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png') return cb(null, true);
+    cb(new Error('LOI DINH DANG'));
+};
+
+const uploadMiddle = multer({ storage, limits, fileFilter }).single('avatar');
 
 const app = express();
 app.listen(3000, () => console.log('Server started'));
@@ -11,6 +23,9 @@ app.set('views', './views');
 
 app.get('/', (req, res) => res.render('home'));
 
-app.post('/upload', uploadMiddle, (req, res) => {
-    res.send('THEM_THANH_CONG');
+app.post('/upload', (req, res) => {
+    uploadMiddle(req, res, err => {
+        if (err) return res.send(`LOI ${err}`);
+        res.send('THANH_CONG');
+    });
 });
